8f23ee4c52837bee37b740511d206b331395b487
mad <mad@PROELE83>
2024-02-07 10:25:58 +0100

    Add project files.

---
+++ Person/Program.cs
@@ -0,0 +1,23 @@
+﻿using Person.Math;
+
+namespace Person
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var mads = new person();
+            mads.FirstName = "Mads";
+            mads.LastName = "Dockir";
+            mads.Introduse();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
---
+++ Person/person.cs
@@ -0,0 +1,15 @@
+﻿namespace Person
+{
+    public class person
+    {
+        public string FirstName;
+        public string LastName;
+
+
+        public void Introduse () 
+        {
+            Console.WriteLine("My name is " + FirstName + " " + LastName);
+        }
+
+    }
+}
